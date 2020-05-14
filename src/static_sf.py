cda743d04973724ac66dc39550ad5fdcbdb2f0fd
Luke Stanek <31392821+lukestanek@users.noreply.github.com>
2020-05-14 12:30:44 -0400

    Update static_sf.py
    fixing .real bug

--- src/static_sf.py
+++ src/static_sf.py
@@ -37,13 +37,13 @@ def ssk(L, k, data, Nconfigs, Nparta, Npartb = 0, sim="single"):
     
     if sim == "single":
         Sout = np.zeros([len(k)])
-        Sout = S.real()
+        Sout = S.real
         
     if sim == "mixture":
         Sout = np.zeros([len(k), 3])
-        Sout[:,0] = Sii.real()
-        Sout[:,1] = Sjj.real()
-        Sout[:,2] = Sij.real()
+        Sout[:,0] = Sii.real
+        Sout[:,1] = Sjj.real
+        Sout[:,2] = Sij.real
         
     return k, k_counts, Sout
 
