5fa317cd6629ac9db565b3e7efcaa35c5471873b
lukestanek <staneklu@msu.edu>
2020-05-22 12:25:36 -0400

    Udated VACF and ICCF (fixed indexing bugs) and added comments.

--- src/iccf.py
+++ src/iccf.py
@@ -1,7 +1,10 @@
 import numpy as np
 import numba
+import time
+import sys
 
-def interdiffusion_current(data, Nconfigs, dt, Nparta, Npartb):
+
+def iccf_compute(data, Nconfigs, dt, Nparta, Npartb):
     
     '''
     Computes the interdiffusion current correlation function for artomistic data in [id x y z vx vy vz] format. 
@@ -24,43 +27,65 @@ def interdiffusion_current(data, Nconfigs, dt, Nparta, Npartb):
     Nartb : int
         Number of particles of type b.
 
+    Returns
+    -------
+    t : array_like
+        Time array with dt = timestep.
         
-    sim : string
-        Simulation type. Can be 'single' or 'binary'    
+    ICCFavg : array_like
+        Averaged (over t0) intrdiffusion current correlation function.
     
+    Dij : array_like
+        Integgral of the ICCF up to the corresponding point in time.
+        Note that this is simply the integral of the autocorrelation function 
+        and not the interdiffusion coefficient. To compute the interdiffusion 
+        coefficient, multilpy by the necessary prefactor which can be 
+        found here: https://link.aps.org/doi/10.1103/PhysRevE.90.023104
+        
     '''
+    
+    # To be changed
+    Nlags = Nconfigs
     
     # Allocate
     jdot = np.zeros([Nlags, Nconfigs])
 
-    # Pick out each species
+        
     a, b, = np.unique(data[:,0])
+
     ra_inds = np.where(data[:,0] == a)
-    ra_array = data[ra_inds,4:7]
+    ra_array = data[ra_inds, :]
     rb_inds = np.where(data[:,0] == b)
-    rb_array = data[rb_inds,4:7]
+    rb_array = data[rb_inds, :]
     
     # Compute concentration
     X1 = Nparta/(Nparta + Npartb)
     X2 = Npartb/(Nparta + Npartb)
     
     # Compute ICCF
-    ICCF = interdiffusion_current(ra_array[0,:,1:], rb_array[0,:,1:], jdot, Nparta, X1, Npartb, X2, Nconfigs):
+    ICCF = iccf_loop(ra_array[0,:,:], rb_array[0,:,:], jdot, Nparta, X1, Npartb, X2, Nconfigs)
         
+    # Average over lags
+    ICCFavg = np.sum(ICCF,  axis=0)/(np.ones(Nconfigs)*range(Nconfigs, 0, -1))
         
     # Create t array
     t = np.linspace(0, Nconfigs, Nconfigs)*dt
     
-    return t, ICCF
+    # Compute integral of correlation function - still need to multiply by prefactor
+    Dij = np.zeros(len(ICCFavg))
+    for step in range(1,len(ICCFavg)):
+        Dij[step] = np.trapz(ICCFavg[0:step], dx=dt)
+        
+    return t, ICCFavg, Dij
 
 @numba.jit
-def interdiffusion_current(ra_array, rb_array, jdot, Nparta, X1, Npartb, X2, Nconfigs):
+def iccf_loop(ra_array, rb_array, jdot, Nparta, X1, Npartb, X2, Nconfigs):
     Nlags = Nconfigs
 
     # Lag loop - exploit stationarity
     for lag in range(Nlags):
-        if lag%10 == 0:
-            print(lag)
+        if lag%(int(Nlags/10)) == 0:         
+            print(lag/Nlags * 100,"% Complete.")
             
         # Loop over snapshot in simulation
         for Nsnap in range(Nconfigs-lag):
@@ -69,17 +94,17 @@ def interdiffusion_current(ra_array, rb_array, jdot, Nparta, X1, Npartb, X2, Nco
             starta = lag*Nparta + Nsnap*Nparta 
             enda = lag*Nparta + (Nsnap + 1)*Nparta
             
-            # Start and end points in simulation data for particle type a
+            # Start and end points in simulation data for particle type b
             startb = lag*Npartb + Nsnap*Npartb
             endb = lag*Npartb + (Nsnap + 1)*Npartb
 
-            # Compute normanlization factor
-            j0 = X2*np.sum(ra_array[0, lag*Nparta:lag*Nparta+ Nparta, :], axis=0) - X1* np.sum(rb_array[0, lag*Npartb:lag*Npartb+Npartb, :], axis=0)
+            # Compute j(0)
+            j0 = X2*np.sum(ra_array[lag*Nparta:lag*Nparta + Nparta, 4:7], axis=0) - X1* np.sum(rb_array[lag*Npartb:lag*Npartb + Npartb, 4:7], axis=0)
         
             # Compute j(t)
-            jt = X2*np.sum(ra_array[0, starta:enda, :], axis=0) - X1*np.sum(rb_array[0, startb:endb, :], axis = 0)
+            jt = X2*np.sum(ra_array[starta:enda, 4:7], axis=0) - X1*np.sum(rb_array[startb:endb, 4:7], axis = 0)
 
-            # Compute normalized correlation function
-            jdot[lag, Nsnap] = np.dot(jt, j0)/np.dot(j0, j0)
+            # Compute correlation function
+            jdot[lag, Nsnap] = np.dot(jt, j0)
 
     return jdot
--- src/vacf.py
+++ src/vacf.py
@@ -8,8 +8,9 @@ import sys
 def vacf_compute(data, Nconfigs, dt, Nparta, Npartb=0, sim='single'):
     
     '''
-    Computes the velocity autocorrelation function for artomistic data in [id x y z vx vy vz] format. 
-    Exploits particle averaging and stationarity for imrovd statistics.
+    Computes the velocity autocorrelation function for artomistic data in 
+    [id x y z vx vy vz] format. Exploits particle averaging and stationarity 
+    for improved statistics.
     
     Parameters
     ----------
@@ -29,10 +30,21 @@ def vacf_compute(data, Nconfigs, dt, Nparta, Npartb=0, sim='single'):
         Timestep
         
     sim : string
-        Simulation type. Can be 'single' or 'binary'    
+        Simulation type. Can be 'single' or 'binary'
+        
+    Returns
+    -------
+    t : array_like
+        Time array with dt = timestep.
+        
+    vavg : array_like
+        Averaged (over particles and t0) veelocity autocrrelation function.
+    
+    D : array_like
+        Self-diffusion coefficient computed via Green-Kubo relation in 3D.
+        The VACF is integrated up to the corresponding point in time.
     
     '''
-    
 
     start = time.time()
     sys.stdout.write('Computing VACF.')
@@ -43,17 +55,20 @@ def vacf_compute(data, Nconfigs, dt, Nparta, Npartb=0, sim='single'):
     
 
     if sim == 'single':
-    
+        
         # Allocate
         vdot = np.zeros([Nlags, Nconfigs])
         vavg = np.zeros(Nconfigs)
         
+        # Comppute vacf
         t, V = vacf_loop(data, vdot, Nparta, Nconfigs, dt, Nlags, lagstep=1)
 
+        # Average over lags
         vavg = np.sum(V,  axis=0)/(np.ones(Nconfigs)*range(Nconfigs, 0, -1))
         
     elif sim == 'binary':
         
+        # Separate data
         a, b, = np.unique(data[:,0])
 
         ra_inds = np.where(data[:,0] == a)
@@ -66,8 +81,11 @@ def vacf_compute(data, Nconfigs, dt, Nparta, Npartb=0, sim='single'):
         vdotb = np.zeros([Nlags, Nconfigs])
         vavg = np.zeros([Nconfigs, 2])
         
-        t, Va, Vb = multi_vacf_loop(ra_array[0,:,1:], rb_array[0,:,1:], vdota, vdotb, Nparta, Npartb, Nconfigs, dt, Nlags, lagstep=1)
-
+        # Compute vacf of species a and b
+        t, Va, Vb = multi_vacf_loop(ra_array[0,:,:], rb_array[0,:,:], vdota, vdotb, 
+                                    Nparta, Npartb, Nconfigs, dt, Nlags, lagstep=1)
+        
+        # Average over the lags
         vavga = np.sum(Va,  axis=0)/(np.ones(Nconfigs)*range(Nconfigs, 0, -1))
         vavgb = np.sum(Vb,  axis=0)/(np.ones(Nconfigs)*range(Nconfigs, 0, -1))
         
@@ -78,16 +96,22 @@ def vacf_compute(data, Nconfigs, dt, Nparta, Npartb=0, sim='single'):
     sys.stdout.write(' Finished.\n')
     print('Time Elapsed: {:.2f} seconds.'.format(end-start))
     
-    # Check for equilibration
-    if (vavg[1] - vavg[0])/(t[1] - t[0]) > 0.1:
-        print("Warning: Slope of Z(0) >= 0.1, consider checking equilibration.")
+#     # Check for equilibration
+#     if (vavg[1] - vavg[0])/(t[1] - t[0]) > 0.1:
+#         print("Warning: Slope of Z(0) >= 0.1, consider checking equilibration.")
         
     # Compute diffusion coefficiet from Green-Kubo formulation in 3D
-    D = np.zeros(len(vavg))
-    for step in range(1,len(vavg)):
-        D[step] = 1/3 * np.trapz(vavg[0:step], dx=dt)
-        
-    return t, V, vavg, D
+    if sim=='single':
+        D = np.zeros(len(vavg))
+        for step in range(1, len(vavg)):
+            D[step] = 1/3 * np.trapz(vavg[0:step], dx=dt)
+    elif sim=='binary':
+        D = np.zeros([Nconfigs, 2])
+        for sp in range(2):
+            for step in range(1, Nconfigs):
+                D[step, sp] = 1/3 * np.trapz(vavg[0:step, sp], dx=dt)        
+
+    return t, vavg, D
     
 @numba.jit
 def vacf_loop(data, vdot, Npart, Nconfigs, dt, Nlags, lagstep=1,):   
@@ -106,7 +130,8 @@ def vacf_loop(data, vdot, Npart, Nconfigs, dt, Nlags, lagstep=1,):
             for Nsnap in range(Nconfigs-lag*lagstep):
                 
                 # Compute the autocorrelation function
-                vdot[lag, Nsnap] += np.dot(data[start_point + Npart*Nsnap + i, 4:7], data[start_point + i, 4:7])#/ np.dot(data[start_point + i, 4:7], data[start_point + i, 4:7])
+                vdot[lag, Nsnap] += np.dot(data[start_point + Npart*Nsnap + i, 4:7], 
+                                           data[start_point + i, 4:7])
 
     # Create t array
     t = np.linspace(0, Nconfigs, Nconfigs)*dt
@@ -115,7 +140,8 @@ def vacf_loop(data, vdot, Npart, Nconfigs, dt, Nlags, lagstep=1,):
     return t, vdot/Npart
 
 @numba.jit
-def multi_vacf_loop(ra_array, rb_array, vdota, vdotb, Nparta, Npartb, Nconfigs, dt, Nlags, lagstep=1):   
+def multi_vacf_loop(ra_array, rb_array, vdota, vdotb, Nparta, Npartb, Nconfigs, dt, 
+                    Nlags, lagstep=1):   
     
     # Loop over particles a
     print('\nLooping over particles of type a')
@@ -133,7 +159,8 @@ def multi_vacf_loop(ra_array, rb_array, vdota, vdotb, Nparta, Npartb, Nconfigs, 
             for Nsnap in range(Nconfigs-lag*lagstep):
 
                 # Compute the autocorrelation function
-                vdota[lag, Nsnap] += np.dot(ra_array[start_point + Nparta*Nsnap + i, 4:7], ra_array[start_point + i, 4:7])#/ np.dot(ra_array[start_point + i, 4:7], ra_array[start_point + i, 4:7])
+                vdota[lag, Nsnap] += np.dot(ra_array[start_point + Nparta*Nsnap + i, 4:7], 
+                                            ra_array[start_point + i, 4:7])
                 
     # Loop over particles b
     print('Looping over particles of type b')
@@ -151,10 +178,9 @@ def multi_vacf_loop(ra_array, rb_array, vdota, vdotb, Nparta, Npartb, Nconfigs, 
             for Nsnap in range(Nconfigs-lag*lagstep):
 
                 # Compute the autocorrelation function
-                vdotb[lag, Nsnap] += np.dot(rb_array[start_point + Npartb*Nsnap + i, 4:7], rb_array[start_point + i, 4:7])#/ np.dot(rb_array[start_point + i, 4:7], rb_array[start_point + i, 4:7])
-
+                vdotb[lag, Nsnap] += np.dot(rb_array[start_point + Npartb*Nsnap + i, 4:7], 
+                                            rb_array[start_point + i, 4:7])
     # Create t array
     t = np.linspace(0, Nconfigs, Nconfigs)*dt
     
-            
     return t, vdota/Nparta, vdotb/Npartb 
